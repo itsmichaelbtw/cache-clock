e7caa1e5664401902bb65d754d90f47638e0546d
Michael <me@miikey.pw>
2022-11-08 18:25:48 +1100

    build: rollup pipelines with cjs, esm and umd support

---
+++ rollup.config.js
@@ -0,0 +1,56 @@
+import resolve from "@rollup/plugin-node-resolve";
+import babel from "@rollup/plugin-babel";
+import bundleSize from "rollup-plugin-bundle-size";
+
+import packageJson from "./package.json";
+
+const extensions = [".ts"];
+const banner = `/**
+    * ${packageJson.name} v${packageJson.version}
+    * ${packageJson.homepage}
+    * (c) ${new Date().getFullYear()} ${packageJson.author}
+    * @license ${packageJson.license}
+    */
+`;
+
+export default {
+    input: "lib/cache-clock.ts",
+    output: [
+        {
+            file: packageJson.main,
+            format: "cjs",
+            sourcemap: true,
+            exports: "named",
+            generatedCode: {
+                constBindings: true
+            },
+            banner: banner
+        },
+        {
+            file: packageJson.module,
+            format: "esm",
+            sourcemap: true,
+            exports: "named",
+            banner: banner
+        },
+        {
+            file: "dist/cache-clock.js",
+            format: "umd",
+            name: "CacheClock",
+            sourcemap: true,
+            exports: "named",
+            banner: banner
+        }
+    ],
+    plugins: [
+        resolve({ extensions }),
+        babel({
+            babelHelpers: "bundled",
+            include: ["lib/**/*.ts"],
+            extensions: extensions,
+            exclude: "node_modules/**",
+            presets: ["@babel/preset-typescript", "@babel/preset-env"]
+        }),
+        bundleSize()
+    ]
+};
